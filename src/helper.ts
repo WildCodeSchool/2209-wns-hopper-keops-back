51494c184b532eb2d5ec86b9868838927c71e060
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2022-12-23 09:40:57 +0100

    Update helper

--- src/helper.ts
+++ src/helper.ts
@@ -1,10 +1,13 @@
-export function mergeObject(object1: object, object2: object): object {
-  const newObject = {};
-  // for (const key of Object.keys(object1)) {
-  //   newObject[key] = object1[key];
-  // }
-  // for (const key of Object.keys(object2)) {
-  //   newObject[key] = object2[key];
-  // }
+export function mergeObject(
+  object1: { [key: string | number]: unknown },
+  object2: { [key: string | number]: unknown }
+): object {
+  const newObject: { [key: string | number]: unknown } = {};
+  for (const key of Object.keys(object1)) {
+    newObject[key] = object1[key];
+  }
+  for (const key of Object.keys(object2)) {
+    newObject[key] = object2[key];
+  }
   return newObject;
 }

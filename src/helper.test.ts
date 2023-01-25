913373b22fd392ef43a766f58b5d4136b36597e9
g-emeline <emelineg@polydea.com>
2023-01-25 14:43:39 +0100

    saved

--- src/helper.test.ts
+++ src/helper.test.ts
@@ -3,14 +3,14 @@ import { mergeObject } from "./helper";
 
 describe("Get merge object", () => {
   it("The first object et the second are merged in one object", () => {
-    expect(mergeObject({ a: 1, b: "3" }, { c: true })).toBe({
+    expect(mergeObject({ a: 1, b: "3" }, { c: true })).toStrictEqual({
       a: 1,
       b: "3",
       c: true,
     });
   });
   it("The first object is overwrite by the second", () => {
-    expect(mergeObject({ a: 1, b: "3" }, { a: true })).toBe({
+    expect(mergeObject({ a: 1, b: "3" }, { a: true })).toStrictEqual({
       a: true,
       b: "3",
     });
@@ -18,8 +18,8 @@ describe("Get merge object", () => {
   it("All the object in are the same out", () => {
     const object1 = { a: 1, b: "3" };
     const object2 = { c: true };
-    expect(mergeObject(object1, object2)).toBe({ a: 1, b: "3", c: true });
-    expect(object1).toBe({ a: 1, b: "3" });
-    expect(object2).toBe({ c: true });
+    expect(mergeObject(object1, object2)).toStrictEqual({ a: 1, b: "3", c: true });
+    expect(object1).toStrictEqual({ a: 1, b: "3" });
+    expect(object2).toStrictEqual({ c: true });
   });
 });
